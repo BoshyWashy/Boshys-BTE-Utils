a9ec868382064b0b31c8eed43f0cf169be1a2b4a
Josh <boshywashy8@gmail.com>
2026-02-19 21:23:49 +0000

    Update ClientPlayNetworkHandlerMixin.java

--- src/main/java/com/boshys/bteutils/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/com/boshys/bteutils/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,7 +1,9 @@
-/* package com.boshys.bteutils.mixin;
+package com.boshys.bteutils.mixin;
 
 import com.boshys.bteutils.BoshysBTEUtils;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.c2s.play.CommandExecutionC2SPacket;
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,24 +13,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
 
-    // Try injecting at HEAD instead of TAIL, and also log if it's being called
-    @Inject(method = "onPlayerPositionLook", at = @At("HEAD"))
-    private void onPlayerPositionLook(PlayerPositionLookS2CPacket packet, CallbackInfo ci) {
-        System.out.println("[BoshysBTEUtils] onPlayerPositionLook mixin triggered!");
-
-        try {
-            var change = packet.change();
-            var pos = change.position();
-            double x = pos.x;
-            double y = pos.y;
-            double z = pos.z;
-
-            System.out.println("[BoshysBTEUtils] Teleport detected to: " + x + ", " + y + ", " + z);
-            BoshysBTEUtils.onPlayerTeleported(x, y, z);
-        } catch (Exception e) {
-            System.out.println("[BoshysBTEUtils] Error in mixin: " + e.getMessage());
-            e.printStackTrace();
+    // Track when player sends a command packet
+    @Inject(method = "sendPacket", at = @At("HEAD"), cancellable = false)
+    private void onSendPacket(net.minecraft.network.packet.Packet<?> packet, CallbackInfo ci) {
+        if (packet instanceof CommandExecutionC2SPacket commandPacket) {
+            String command = commandPacket.command();
+            if (BoshysBTEUtils.INSTANCE != null) {
+                BoshysBTEUtils.INSTANCE.onCommandSent(command);
+            }
         }
     }
 }
-*/
